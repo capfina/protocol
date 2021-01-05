0c9cc874d26044fe79200600a0f04e5db044de4c
RepoContributor <repo-contributor@users.noreply.github.com>
2021-01-05 14:30:20 -0500

    add deploy:proxy task that allows deploying an implementation contract, deploying a proxy and transferring admin to governance

--- tasks/deployment.js
+++ tasks/deployment.js
@@ -2,6 +2,7 @@ const { task, subtask } = require('hardhat/config');
 const deploySystem = require('../lib/deployment/deploySystem');
 const upgradeSystem = require('../lib/deployment/upgradeSystem');
 const deployImpl = require('../lib/deployment/deployImpl');
+const deployProxy = require('../lib/deployment/deployProxy');
 
 subtask('deploy:system', 'deploys system')
   .setAction(deploySystem);
@@ -13,10 +14,18 @@ task('deploy', 'deploys system')
   });
 
 task('deploy:impl', 'deploys contract implementation')
-  .addParam("contract", "contract name")
+  .addParam('contract', 'contract name')
   .setAction(async (params, hre) => {
     await run('compile');
     await deployImpl(params, hre);
+  });
+
+task('deploy:proxy', 'deploys an implementation contract and its proxy')
+  .addParam('contract', 'contract name')
+  .addParam('governance', 'governance contract address')
+  .setAction(async (params, hre) => {
+    await run('compile');
+    await deployProxy(params, hre);
   });
 
 task('upgrade', 'upgrades system')
